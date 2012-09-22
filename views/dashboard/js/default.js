952891374d9e853a84d11563be1d3b3b4ecc02b5
Rafael <rafaeleliasw@gmail.com>
2012-09-22 14:11:46 -0300

    cambio la funcion de limpiar formularios a un js general

--- views/dashboard/js/default.js
+++ views/dashboard/js/default.js
@@ -1,43 +1,50 @@
 $(function() {
 	
-	$.get('dashboard/xhrGetListings', function(o) {     
+    $.get('dashboard/xhrGetListings', function(o) {     
                 
-                $('#listInserts').append('<thead><tr> <th style="width: 5%;">ID</th> <th style="width: 55%;">Text</th> <th style="width: 40%;">Acciones</th> </tr></thead>');
+        $('#listInserts').append('<thead><tr> <th style="width: 5%;">ID</th> <th style="width: 55%;">Text</th> <th style="width: 40%;">Acciones</th> </tr></thead>');
                 
-		for (var i = 0; i < o.length; i++)
-		{
-			$('#listInserts').append('<tr> <td>' + o[i].id + '</td><td>' + o[i].text + '</td><td><a class="mod" rel="'+o[i].id+'" href="#"><i class="icon-edit"></i> Modificar</a> <a class="del" rel="'+o[i].id+'" href="#"> <i class="icon-remove-circle"></i> Borrar</a></td></tr>');
-		}
-                $('#listInserts').append('</table>');
+        if (o.length >= 1) {
+            for (var i = 0; i < o.length; i++)
+            {
+                $('#listInserts').append('<tr> <td>' + o[i].id + '</td><td>' + o[i].text + '</td><td><a class="mod" rel="'+o[i].id+'" href="#"><i class="icon-edit"></i> Modificar</a> <a class="del" rel="'+o[i].id+'" href="#"> <i class="icon-remove-circle"></i> Borrar</a></td></tr>');
+            }
+        } else {
+            $('#listInserts').append('<tr><td colspan="3" style="text-align: center;">No se han encontrado registros </td></tr>');
+        }
+            
 		
-		$('.del').live('click', function() {
-			delItem = $(this);
-			var id = $(this).attr('rel');
+        $('.del').live('click', function() {
+            delItem = $(this);
+            var id = $(this).attr('rel');
 			
-			$.post('dashboard/xhrDeleteListing', {'id': id}, function(o) {
+            $.post('dashboard/xhrDeleteListing', {
+                'id': id
+            }, function(o) {
 				
-			}, 'json');
+                }, 'json');
 			
-                        alert('El registro se eliminó correctamente');
-                        location.reload();
-			return false;
-		});
+            alert('El registro se eliminó correctamente');
+            location.reload();
+            return false;
+        });
 		
-	}, 'json');
+    }, 'json');
 	
 	
 	
-	$('#randomInsert').submit(function() {
-		var url = $(this).attr('action');
-		var data = $(this).serialize();
+    $('#randomInsert').submit(function() {
+        var url = $(this).attr('action');
+        var data = $(this).serialize();
 		
-		$.post(url, data, function(o) {
-			$('#listInserts').append('<tr> <td>' + o.id + '</td><td>' + o.text + '</td><td><a class="mod" rel="'+o.id+'" href="#"><i class="icon-edit"></i> Modificar</a> <a class="del" rel="'+o.id+'" href="#"><i class="icon-remove-circle"></i> Borrar</a></td></tr>');		
-                        $('#text').val("");
-		}, 'json');
+        $.post(url, data, function(o) {
+            alert('El registro se agregó correctamente');
+            location.reload();		
+            $('#text').val("");
+        }, 'json');
 		
 		
-		return false;
-	});
+        return false;
+    });
 
 });
